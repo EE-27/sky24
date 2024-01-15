0f5d4d712b76641eb9cc3ab5351a272c9edc00be
JL34 <johnny2791@seznam.cz>
2024-01-15 14:05:35 +0100

    Task 3 1.0: Describe CRUD for course and lesson models, but for course do it via ViewSets, and for lesson - via Generic classes.
    For the operation of controllers, describe the simplest serializers.
    The work of each endpoint should be checked with Postman.
    Also at this stage of work we do not care about security and do not close objects and models
    from editing even by the simplest authorisation.

--- school/serializers.py
+++ school/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
-from school.models import Course
+from school.models import Course, Lesson
 
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
 # pro ViewSet stačí takto:
 # je potřeba nainstalovat applikaci Postman >>https://dl.pstmn.io/download/latest/win64<<
 # zapnout ji, zapnout django server a do url v applikaci napsat http://localhost:8000/courses/
 # pak jde GET, POST, PUT, DELETE atd...
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+# pro generics takto:
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
--- school/views.py
+++ school/views.py
@@ -1,11 +1,37 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from school.models import Course
-from school.serializers import CourseSerializer
+from school.models import Course, Lesson
+from school.serializers import CourseSerializer, LessonSerializer
 
 
+# pro ViewSet stačí takto:
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
-# pro ViewSet stačí takto
+
+# pro generics takto:
+class LessonCreateAPIView(generics.CreateAPIView):  # POST
+    serializer_class = LessonSerializer
+    # neni potřeba queryset
+
+
+class LessonListAPIView(generics.ListAPIView):  # GET
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):  # GET
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):  # PATCH (může být 1 field)
+    serializer_class = LessonSerializer             # PUT (chce všechno)
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):  # DELETE
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()  # Maslov ve videu o Generics tenhle řádek nemá,
+                                     # mně to nešlo bez něj ¯\_(ツ)_/¯
