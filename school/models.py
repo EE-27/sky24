53c129d3defac927b97d22f061dbb56211d84553
JL34 <johnny2791@seznam.cz>
2024-01-18 11:18:08 +0100

    Task 2     Add a new model "Payments" with the following fields: * user, * payment date, * paid course or lesson, * payment amount, * payment method: cash or transfer to account.
        Write data to this model via the fixtur tool or custom command.

--- school/models.py
+++ school/models.py
@@ -29,3 +29,44 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Lesson"
         verbose_name_plural = "Lessons"
+
+
+class Payments(models.Model):
+    # zapsáno do modolu pomocí "fixtur tool", to znaméná: vytvořen .json a potom: python manage.py loaddata payments
+    # formát pro json:
+    # [
+    #     {
+    #         "model": "school.payments", \\ jméno app a jméno modelu
+    #         "pk": 1, \\ vždy změnit, jinak se jenom přepisuje
+    #         "fields": {
+    #             "user": "Honza",
+    #             "payment_date": "2024-01-18", \\ YYYY-MM-DD
+    #             "course_or_lesson": "course", \\ "course" nebo "lesson"
+    #             "payment_amount": 500,
+    #             "payment_method": "cash" \\ "cash" nebo "transfer"
+    #         }
+    #     }
+    # ]
+
+    COURSE_OR_LESSON = [
+        ("course", "Course"),
+        ("lesson", "Lesson")
+    ]
+    PAYMENT_METHOD = [
+        ("cash", "Cash"),
+        ("transfer", "Transfer")
+    ]
+    user = models.CharField(max_length=24, verbose_name="User")
+    payment_date = models.DateField(verbose_name="Payment date")
+    course_or_lesson = models.CharField(max_length=12, verbose_name="Paid course or lesson?", choices=COURSE_OR_LESSON,
+                                        null=True, blank=True)
+    payment_amount = models.IntegerField(verbose_name="Payment amount")
+    payment_method = models.CharField(max_length=12, verbose_name="Payment method", choices=PAYMENT_METHOD, null=True,
+                                      blank=True)
+
+    def __str__(self):
+        return f"{self.user} - method: {self.payment_method} - amount: {self.payment_amount}"
+
+    class Meta:
+        verbose_name = "Payment"
+        verbose_name_plural = "Payments"
