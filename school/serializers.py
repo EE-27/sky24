d362e95f513ef5abc44f02f712275288240b6ee9
JL34 <johnny2791@seznam.cz>
2024-01-18 11:43:18 +0100

    Task 3     Implement a lesson output field for the course model serializer.

--- school/serializers.py
+++ school/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 
 from school.models import Course, Lesson
 
+# pro generics takto:
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 # pro ViewSet stačí takto:
 # je potřeba nainstalovat applikaci Postman >>https://dl.pstmn.io/download/latest/win64<<
 # zapnout ji, zapnout django server a do url v applikaci napsat http://localhost:8000/courses/
@@ -11,6 +18,11 @@ class CourseSerializer(serializers.ModelSerializer):
     # počítat kolik lekcí má jeden course - přidat toto:
     lesson_count = serializers.SerializerMethodField()
 
+    # přidat výstup pro lekce: LessonSerializer hodit nahoru, protože ho tady callujeme
+    # pak přidat source, zase díky: related_name="lessons", abych mohl callovat lekce přes Course
+    # a many=True, protože Maslov...
+    lesson = LessonSerializer(source="lessons", many=True)
+
     class Meta:
         model = Course
         fields = "__all__"
@@ -19,10 +31,5 @@ class CourseSerializer(serializers.ModelSerializer):
         # related_name="lessons", abych mohl callovat lekce přes Course
         return lessons_count.lessons.count()
 
-# pro generics takto:
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
 
 
